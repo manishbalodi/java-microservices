059dedf60b4853fedc32c424f7ad7ba945964eb4
manishbalodi <manish.balodi.1998@gmail.com>
2021-10-18 16:26:29 +0530

    added Eureka server

---
+++ discovery-server/src/test/java/com/manish/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
